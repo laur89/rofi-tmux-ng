e9271203095bddf208ef6a36f5f24c102ffcb739
laur <laur.aliste@gmail.com>
2024-11-04 16:44:39 +0100

    tmux2: session.attached -> .attached_window

--- rft/tmux2.py
+++ rft/tmux2.py
@@ -66,6 +66,6 @@ class tmux(object):
     def _get_cur_session(self) -> libtmux.session.Session:
         """Return reference to our current tmux session."""
         for s in self._sessions:
-            if str(s.attached) != '0':
+            if str(s.attached_window) != '0':
                 return s
         return None
